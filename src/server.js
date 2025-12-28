e3a056d7366396767134329df0cb79583393cc99
Khumo <qu.modisane06@gmil>
2025-12-28 09:38:22 +0200

    Add express.json() middleware before routes

--- src/server.js
+++ src/server.js
@@ -28,6 +28,9 @@ app.use(cors({
   ],
   credentials: true
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Static files (for future frontend)
 app.use(express.static(path.join(__dirname, '../public')));
